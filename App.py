ea0f06c50e411c1e71325936bd0e4ccf2b3b4ee8
Nannpy <131631162+Nannpy@users.noreply.github.com>
2024-06-23 17:33:01 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,11 +1,16 @@
 import streamlit as st
 
-st.title("T-Rex Runner Game")
+col1, col2 = st.beta_columns(2) 
 
-game_url = "https://vianroyal.github.io/t-rex-runner/"
-st.markdown(
-    f"""
-    <iframe src="{game_url}" width="800" height="600" frameborder="0"></iframe>
-    """,
-    unsafe_allow_html=True
-)
+with col1 :
+    st.title("T-Rex Runner Game")
+    game_url = "https://vianroyal.github.io/t-rex-runner/"
+    st.markdown(
+        f"""
+        <iframe src="{game_url}" width="800" height="600" frameborder="0"></iframe>
+        """,
+        unsafe_allow_html=True
+    )
+
+with col2:
+    st.header('Face Excersice interface')
